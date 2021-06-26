b5b28078af5866b650ac74e415478994b7e4c25b
Rajat Patwari <rajatpatwari@gmail.com>
2021-06-26 15:52:30 -0400

    Change the server to TypeScript

---
+++ server/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import helmet from "helmet";
+import path from "path";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+      "script-src": [ "'self'", "'unsafe-inline'", "rajatpatwari.com" ]
+    }
+  }
+}));
+
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
+app.get("/*", (_, res) => res.send(path.join(buildPath, "index.html")));
+
+const port = 3000;
+app.listen(port, () => console.log(`Server started on port ${port}...`));
